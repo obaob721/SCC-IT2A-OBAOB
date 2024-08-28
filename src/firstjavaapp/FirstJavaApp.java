45d40c308cfe809a32b06970efc2ccc76e6b92b7
lawrencesumbi <guiansumbi@gmail.com>
2024-08-28 12:15:14 +0800

    Product Array

--- src/firstjavaapp/FirstJavaApp.java
+++ src/firstjavaapp/FirstJavaApp.java
@@ -6,12 +6,43 @@ public class FirstJavaApp {
   //      Grades grd = new Grades();
  //       grd.getGrade();
  
- Product pr = new Product();
- pr.addProduct(10011, "Soap", 10, 20);
- pr.viewProduct();
+ //Product pr = new Product();
+ //pr.addProduct(10011, "Soap", 10, 20);
+ //pr.viewProduct();
  
- Product pr1 = new Product();
- pr1.addProduct(10012,"Cards", 0, 50);
- pr1.viewProduct();
+ //Product pr1 = new Product();
+ //pr1.addProduct(10012,"Cards", 0, 50);
+ //pr1.viewProduct();
+   Scanner sc = new Scanner(System.in);
+   Product[] pr = new Product[100];
+   
+   int nump, i;
+        System.out.print("Enter no. of products: ");
+        nump = sc.nextInt();
+        double ttp = 0, totalTEP = 0;
+        for(i = 0; i<nump;i++){
+            System.out.println("Enter Details of Products: "+(i+1)+": ");
+            System.out.print("Id: ");
+            int id = sc.nextInt();
+            System.out.print("Name: ");
+            String name = sc.next();
+            System.out.print("Price: ");
+            double prc = sc.nextDouble();
+            System.out.print("Stock: ");
+            int st = sc.nextInt();
+            System.out.print("Sold: ");
+            int sold = sc.nextInt();
+            
+            pr[i] = new Product();
+            pr[i].addProduct(id, name, prc, st, sold);
+        }
+        for(i = 0; i<nump;i++){
+        pr[i].viewProduct();
+        
+         ttp = ttp + (pr[i].price * pr[i].sold);
+         totalTEP = totalTEP + pr[i].price * pr[i].stocks; 
+        }
+        System.out.println("Total Profit: "+ttp);
+        System.out.println("Total TEP: "+totalTEP);
 }         
 }
--- src/firstjavaapp/Product.java
+++ src/firstjavaapp/Product.java
@@ -1,25 +1,28 @@
 
+
 package firstjavaapp;
 
 public class Product {
     String name;
-    int pid, stocks;
+    int pid, stocks, sold;
     double price;
 
-    public void addProduct(int id, String pname, int st, double pr ){
+    public void addProduct(int id, String pname, double prc, int st, int sld){
     this.name = pname;
     this.pid = id;
-    this.price = pr;
+    this.price = prc;
     this.stocks = st;
+    this.sold = sld;
    
   }
     public void viewProduct(){
         
         String status = (this.stocks < 1) ? "Out of Stock" : "Available" ;
         double tep = this.price  * this.stocks;
-        System.out.printf("%-10d %-10s %-10d %-10.2f %-10s %-10.2f\n", 
-                this.pid, this.name, this.stocks,this.price ,status, tep);
+        double profit =  this.price * this.sold;
+        
+        System.out.printf("%-10d %-10s %-10d %-10.2f %-10d %-10.2f %-10s %-10.2f\n", 
+                this.pid, this.name, this.stocks,this.price ,this.sold, profit, status, tep);
     }
     
   }
-
